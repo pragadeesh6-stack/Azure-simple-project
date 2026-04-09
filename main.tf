f9d5709cd42ab61afb6997fffa91761d748326e5
Pragadeesh Rajendran <pragadeesh6@gmail.com>
2026-04-09 21:40:52 +1200

    Initial commit with Terraform project

---
+++ main.tf
@@ -0,0 +1,74 @@
+# main.tf
+
+provider "azurerm" {
+  features {}
+}
+
+# 1. Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "tf-demo-rg"
+  location = "australiaeast"
+}
+
+# 2. Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "tf-demo-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# 3. Subnet
+resource "azurerm_subnet" "subnet1" {
+  name                 = "tf-demo-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# 4. Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "demo-vm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# 5. Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "demo-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("C:/Users/USER/.ssh/id_ed25519.pub") # replace with your path if different
+  }
+}
+
+# 6. Output VM public IP
+output "vm_private_ip" {
+  value = azurerm_network_interface.nic.private_ip_address
+}
